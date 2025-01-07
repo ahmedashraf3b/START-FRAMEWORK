cc0c8dea028842f62aa6a3ff4aea1a8fa66803cd
ahmedashraf3b <jacksonxx300@gmail.com>
2025-01-07 21:22:33 +0200

    hhh

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ function active(e) {
 <nav className="border-gray-200 bg-[#2c3e50] fixed top-0 z-40  w-full p-4 ">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-    <Link to='START-FRAMEWORK' className={last == null? "self-center font-semibold whitespace-nowrap text-3xl  text-white":"self-center font-semibold whitespace-nowrap text-3xl  text-white"} onClick={()=> active(null)}>START FRAMEWORK</Link>
+    <Link to='' className={last == null? "self-center font-semibold whitespace-nowrap text-3xl  text-white":"self-center font-semibold whitespace-nowrap text-3xl  text-white"} onClick={()=> active(null)}>START FRAMEWORK</Link>
     </a>
     <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
       <ul className="flex flex-col font-medium mt-4 text-xl rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
